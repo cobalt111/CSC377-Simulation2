14a95e389b277ff2d23aef6da14674a417c24615
JustenC14 <32503421+JustenC14@users.noreply.github.com>
2017-11-06 19:17:06 -0500

    Fixed Null pointers breaking it
    Added a ton of comments and fixed a couple things.

--- Simulation2/FirstFit-Memory.h
+++ Simulation2/FirstFit-Memory.h
@@ -69,39 +69,56 @@ void FFMemory::printList() {
 }
 
 int FFMemory::allocate_memory(int process_id, int num_units) {
-	Node * walkPointer = front->next;
-	int allocateSize = 0;
-	int nodesTraversed_local =  0;
-	if (num_units < 3 || num_units > 10) {
+	Node * walkPointer = front->next;                       //Create a pointer to walk-through memory. Start it 1 node after the header
+	int allocateSize = 0;                                   //allocateSize will keep track of how large the empty space we are traversing is
+	int nodesTraversed_local =  0;                          //nodesTraversed will be returned if the allocation is successful. Used to statistical information
+
+	if (num_units < 3 || num_units > 10) {                  //Ensures that the num_units being used is within the allowed parameters for the simulation
 		return -1;
 	}
-	for (int i = 0; i < numElements; i++) {
-		if (walkPointer->pid == 0) {
-			current = walkPointer; //Save starting location of empty block
-			while (walkPointer->pid == 0) {
-				allocateSize++;
-				if (allocateSize == num_units) {
-					break;
+
+	for (int i = 0; i < numElements; i++) {                 //Begin walking through the memory linked list from the first node
+        allocateSize = 0;                                   //Reset allocate size for the next empty space found.
+		if (walkPointer->pid == 0) {                        //Check the node currently on. If it is empty begin checking to see if it is large enough for the process
+			current = walkPointer;                          //Save starting location of empty block
+                                                            //Current will be returned to if this area is used to write the process to.
+			while (walkPointer->pid == 0) {                 //Start of loop to walk through open space
+
+				allocateSize++;                             //Increase size of allocateSize first because we want this to start at 1, not 0.
+
+				if (allocateSize == num_units) {            //Check to see if allocateSize has counted a space large enough for us to put the process in
+					break;                                  //If it is break this while loop, there is no need to continue counting this huge empty space
 				}
-				walkPointer = walkPointer->next;
-				nodesTraversed_local++;
+				if (walkPointer->next == NULL){             //If allocateSize isn't big enough yet, check to make sure we haven't already hit the end of the memory
+                    denied_allocations++;                   //If we have increase the denied_allocations for statistical reasons
+                    return -1;                              //Return to the main program, there isn't space to put this process as of right now.
+				}
+				walkPointer = walkPointer->next;            //If we haven't reached the end of the string, but we still need more space for the process move to the next space and
+				nodesTraversed_local++;                     //check to see we are still in an empty space. Inc nodesTravelled each time for statistics.
 			}
-			if (allocateSize >= num_units) {
-				walkPointer = current;
-				for (int z = 0; z < num_units; z++) {
+
+			if (allocateSize >= num_units) {                //If we have found a space big enough, prepare to write the new process into memory.
+				walkPointer = current;                      //Return to the start of the empty space
+
+				for (int z = 0; z < num_units; z++) {       //This loop will write the empty space with the process.
 					walkPointer->pid = process_id;
 					walkPointer = walkPointer->next;
 				}
-				num_of_allocations++;
-				nodes_traversed += nodesTraversed_local;
-				return nodesTraversed_local;
+
+				num_of_allocations++;                       //Inc total number of allocations for statistical reasons
+				nodes_traversed += nodesTraversed_local;    //Keep track of the total number of nodes traversed successfully for statistical reasons
+				return nodesTraversed_local;                //return the local amount of nodes traversed for just this allocation to the main program as required in specs
 			}
 
 		}
-		walkPointer = walkPointer->next;
-		nodesTraversed_local++;
+        if (walkPointer->next == NULL){             //If allocateSize isn't big enough yet, check to make sure we haven't already hit the end of the memory
+            denied_allocations++;                   //If we have increase the denied_allocations for statistical reasons
+            return -1;                              //Return to the main program, there isn't space to put this process as of right now.
+        }
+		walkPointer = walkPointer->next;                    //If that area wasn't big enough for the process, or a empty space hasn't been reached yet, continue walking through memory
+		nodesTraversed_local++;                             //Inc nodes traversed each time. This doesn't count as double because when a blank space is found we dont reset walkpointer ever
 	}
-	denied_allocations++;
+	denied_allocations++;                                   //somehow if we make it all the way through the string and end the for loop this catches and counts it as a denial
 	return -1;
 }
 
@@ -121,6 +138,9 @@ int FFMemory::deallocate_mem(int process_id) {
         cout << "DELETING " << process_id << endl;
 		walkPointer->pid = 0;
         cout << "MOVING ON\n";
+        if (walkPointer->next == NULL){
+            break;
+        }
 		walkPointer = walkPointer->next;
 	}
 	return 1;
